251d8bd76ea0e023c5282acf38f0da9285362002
Junya Koseki <junya_koseki@JunyanoMacBook.local>
2019-01-06 13:03:37 +0900

    Update: class name

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path(book), flash: { notice: "Book was successfully created" }
+    redirect_to book_path(book), flash: { notice: "Book was successfully created." }
   end
 
   def edit
@@ -24,13 +24,13 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
+    redirect_to books_path, flash: { notice: "Book was successfully destroyed." }
   end
 
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book), flash: { notice: "Book was successfully updated" }
+    redirect_to book_path(book), flash: { notice: "Book was successfully updated." }
   end
 
   private
